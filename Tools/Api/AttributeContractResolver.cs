02010206b0fbeabbb6c38d34495c2ffa9a0d8048
Sik <contact@sikelio.wtf>
2024-06-24 20:01:51 +0200

    feat: added api request class

---
+++ Tools/Api/AttributeContractResolver.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace FrenchyApps42.Tools.Api
+{
+    public class AttributeContractResolver : DefaultContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            var props = base.CreateProperties(type, memberSerialization);
+
+            foreach (var prop in props)
+            {
+                PropertyInfo? propInfo = type
+                    .GetProperty(prop.UnderlyingName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+                if (propInfo != null)
+                {
+                    JsonPropertyNameAttribute? jsonPropertyName = propInfo.GetCustomAttribute<JsonPropertyNameAttribute>();
+
+                    if (jsonPropertyName != null)
+                    {
+                        prop.PropertyName = jsonPropertyName.Name;
+                    }
+                }
+            }
+
+            return props;
+        }
+    }
+}
---
+++ Tools/Api/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace FrenchyApps42.Tools.Api
+{
+    public struct Result<T>
+    {
+        public int StatusCode;
+        public T? Value;
+        public string? Error;
+
+        public Result(int statusCode, T value, string error = "")
+        {
+            this.StatusCode = statusCode;
+            this.Value = value;
+            this.Error = error;
+        }
+    }
+}
