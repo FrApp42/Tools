472209d99aab7bc6c7db257f66f253dfb833fd02
Sik <contact@sikelio.wtf>
2024-07-24 12:29:36 +0200

    clean(code): removed old projects

--- Shutdowner/Models/CommandResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FrenchyApps42.System.Shutdowner.Models
-{
-    public class CommandResult
-    {
-        public int ExitCode { get; set; }
-
-        public string? ErrorMessage { get; set; }
-    }
-}
